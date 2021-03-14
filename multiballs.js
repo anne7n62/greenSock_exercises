186fe3de10fd87a69f31ac6443e353948617859b
Annemette <anne7n62@stud.kea.dk>
2021-03-14 13:12:47 +0100

    exercise 3 done

--- multiballs.js
+++ multiballs.js
@@ -1,7 +1,6 @@
 "use strict";
 
+// Exercise 2
 gsap.to(".ball", { duration: 2, x: 400, stagger: 0.2, rotate: 200 });
-
 gsap.to(".ball", { delay: 2, scale: 0.5 });
-
 gsap.to(".ball", { yoyo: true, y: -300, repeat: -1 });
